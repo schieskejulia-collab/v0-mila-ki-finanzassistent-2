85331e22ac9f72e8bfa1cb3c771b8f151e4ed90a
schieskejulia-collab <schieskejulia@gmail.com>
2026-06-06 21:16:46 +0200

    page.tsx aktualisieren

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState } from 'react'
-import { TransactionForm } from '../components/transaction-form'
+import { TransactionForm } from '../components/ui/transaction-form'
 import { getTotals, USER } from '../lib/demo-data'
 
 export default function HomePage() {
