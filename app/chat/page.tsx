fc457ac6a592d00493408efc3a3f07020628c1e5
schieskejulia-collab <schieskejulia@gmail.com>
2026-06-19 02:16:35 +0200

    page.tsx aktualisieren

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -4,7 +4,7 @@ import { MilaChat } from "../../components/ui/mila-chat"
 
 export default function ChatPage() {
   return (
-    <div className="min-h-screen bg-[#fbf9ff] p-4">
+    <div className="min-h-[100dvh] overflow-hidden bg-[#fbf9ff] p-4">
       <MilaChat />
     </div>
   )
