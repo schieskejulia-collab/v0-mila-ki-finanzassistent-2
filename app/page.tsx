5aa79d5d0f90616b4ebd6233a4ae62f43511680e
schieskejulia-collab <schieskejulia@gmail.com>
2026-06-24 08:38:41 +0200

    page.tsx aktualisieren

--- app/page.tsx
+++ app/page.tsx
@@ -132,9 +132,7 @@ const overdueIncomes = (incomes || []).filter(
 const totalOpenAmount = openIncomes.reduce((sum, i) => sum + Number(i.amount || 0), 0)
 const totalOverdueAmount = overdueIncomes.reduce((sum, i) => sum + Number(i.amount || 0), 0)
 const openCount = openIncomes.length
-const overdueCount = overdueIncomes.length
-  const totalOpenAmount = openIncomes.reduce((sum, i) => sum + Number(i.amount || 0), 0)
-  const openCount = openIncomes.length
+const overdueCount = overdueIncomes.lengt
 
   const availableInTwoWeeks = (summary.balance || 0) + totalOpenAmount
   const nextPayments = (summary.totalExpenses || 0) * 0.8
