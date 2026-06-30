c975aeae3383dcac7f99b61f73964c176dd3fed9
schieskejulia-collab <schieskejulia@gmail.com>
2026-06-30 21:06:09 +0200

    merchants.ts erstellen

---
+++ lib/merchants.ts
@@ -0,0 +1,31 @@
+export const MERCHANTS = {
+  medimax: {
+    category: 'hardware',
+    deductible: 'likely',
+  },
+
+  "deutsche post": {
+    category: 'versand',
+    deductible: 'likely',
+  },
+
+  "mcdonald's": {
+    category: 'bewirtung',
+    deductible: 'depends',
+  },
+
+  kik: {
+    category: 'bekleidung',
+    deductible: 'depends',
+  },
+
+  deichmann: {
+    category: 'bekleidung',
+    deductible: 'depends',
+  },
+
+  "nanu-nana": {
+    category: 'dekoration',
+    deductible: 'depends',
+  },
+}
